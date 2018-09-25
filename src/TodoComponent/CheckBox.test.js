926cb4839f5a431ed0b5ac8c94681bb703b3e771
Janice Smith <janice.k.n.smith@gmail.com>
2018-09-25 15:50:56 -0400

    adding enzyme

--- src/TodoComponent/CheckBox.test.js
+++ src/TodoComponent/CheckBox.test.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CheckBox from './CheckBox';
 
-it('renders without crashing', () => {
+// it('renders without crashing', () => {
+//
+//   const div = document.createElement('div');
+//   ReactDOM.render(
+//     <CheckBox
+//       checked={false}
+//       disabled={false}
+//       onChange={() => {}}
+//     />,
+//     div
+//   );
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
-  const div = document.createElement('div');
-  ReactDOM.render(
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  const checkBox = shallow(
     <CheckBox
       checked={false}
       disabled={false}
       onChange={() => {}}
-    />,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+    />);
+  expect(checkBox).toBe.ok
 });
