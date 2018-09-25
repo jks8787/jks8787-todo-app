fa7d83179b610df916e400c3244cb90cff602ea3
Janice Smith <janice.k.n.smith@gmail.com>
2018-09-25 16:16:26 -0400

    setting up routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FaqPage from './FaqPage';
+import TodoPage from './TodoPage';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route exact path="/faq" component={FaqPage} />
+      <Route exact path="/todos" component={TodoPage} />
+    </App>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
