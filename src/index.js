fd7c5022c480e4f2c008bfa6aac3ace89c676878
Janice Smith <janice.k.n.smith@gmail.com>
2018-09-27 17:07:53 -0400

    setting up Provider

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import App from './App';
 import FaqPage from './FaqPage';
 import TodoPage from './TodoPage';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+const store = configureStore();
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path="/faq" component={FaqPage} />
-      <Route exact path="/todos" component={TodoPage} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route exact path="/faq" component={FaqPage} />
+        <Route exact path="/todos" component={TodoPage} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
