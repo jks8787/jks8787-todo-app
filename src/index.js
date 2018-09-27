314a77c357439ee4b6bd7ee13cab2b166a09e018
Janice Smith <janice.k.n.smith@gmail.com>
2018-09-27 17:07:53 -0400

    setting up Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+const store = configureStore();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
